64f7758fbbb069f4731260d1733efc920bebb37d
Andreas Atle <atle.andreas@gmail.com>
2026-01-16 14:27:58 -0600

    Wire title and author modal open/close behavior
    Bind Edit title and Edit author buttons to open their respective modals,
    and wire cancel buttons to close them using existing modal helpers.
    Remove legacy modal logic and ensure modals open and close reliably
    without adding submit behavior.

--- src/web/static/js/editor_controller.js
+++ src/web/static/js/editor_controller.js
@@ -2,51 +2,16 @@ import { $ } from "./dom.js";
 import { closeModal, openModal } from "./modals.js";
 
 export function initEditorController() {
-  const titleModal = $("title-modal");
-  const authorModal = $("author-modal");
-
-  function openTitleModal() {
-    openModal("title-modal", "title-modal-input");
-    if (titleModal) {
-      titleModal.addEventListener("keydown", handleTitleEscape);
-    }
-  }
-
-  function closeTitleModal() {
-    if (titleModal) {
-      titleModal.removeEventListener("keydown", handleTitleEscape);
-    }
-    closeModal("title-modal");
-  }
-
-  function openAuthorModal() {
-    openModal("author-modal", "author-modal-input");
-    if (authorModal) {
-      authorModal.addEventListener("keydown", handleAuthorEscape);
-    }
-  }
-
-  function closeAuthorModal() {
-    if (authorModal) {
-      authorModal.removeEventListener("keydown", handleAuthorEscape);
-    }
-    closeModal("author-modal");
-  }
-
-  function handleTitleEscape(event) {
-    if (event.key === "Escape") {
-      closeTitleModal();
-    }
-  }
-
-  function handleAuthorEscape(event) {
-    if (event.key === "Escape") {
-      closeAuthorModal();
-    }
-  }
-
-  $("open-title-modal-btn")?.addEventListener("click", openTitleModal);
-  $("open-author-modal-btn")?.addEventListener("click", openAuthorModal);
-  $("title-cancel-btn")?.addEventListener("click", closeTitleModal);
-  $("author-cancel-btn")?.addEventListener("click", closeAuthorModal);
+  $("open-title-modal-btn")?.addEventListener("click", () =>
+    openModal("edit-title-modal", "edit-title-input"),
+  );
+  $("open-author-modal-btn")?.addEventListener("click", () =>
+    openModal("edit-author-modal", "edit-author-input"),
+  );
+  $("edit-title-cancel-btn")?.addEventListener("click", () =>
+    closeModal("edit-title-modal"),
+  );
+  $("edit-author-cancel-btn")?.addEventListener("click", () =>
+    closeModal("edit-author-modal"),
+  );
 }
